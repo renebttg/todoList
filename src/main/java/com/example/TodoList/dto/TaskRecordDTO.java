d66cf5f1f10bb137c59cd704fb5cb30cfb1f6b4b
renebttg <renebattaglia@hotmail.com>
2025-08-13 11:45:57 -0300

    :sparkles: feat(dto): add TaskRecordDTO for task data transfer

---
+++ src/main/java/com/example/TodoList/dto/TaskRecordDTO.java
@@ -0,0 +1,11 @@
+package com.example.TodoList.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record TaskRecordDTO(
+        @NotBlank String title,
+        @NotNull(message = "Field 'completed' is required.") Boolean completed,
+        @NotNull LocalDateTime time) {}
